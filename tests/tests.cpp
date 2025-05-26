b72bc7d004316a4418d4f97945938ad70d0e76d1
METEOPuT <iklesh32@gmail.com>
2025-05-26 06:37:49 -0400

    Добавлены mock-классы

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,1 +1,18 @@
-a
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "IIterator.h"
+#include "IAggregate.h"
+
+class MockIterator : public IIterator {
+public:
+    MOCK_METHOD0(first, int());
+    MOCK_METHOD0(next, void());
+    MOCK_METHOD0(isDone, bool());
+    MOCK_METHOD0(currentItem, int());
+};
+
+class MockAggregate : public IAggregate {
+public:
+    MOCK_METHOD0(createIterator, IIterator*());
+};
